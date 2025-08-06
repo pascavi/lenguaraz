7f03bd05484a71bb21e9c55b0091eb0fce32af9b
Pascual Cano <pascual@allhuman.com>
2025-08-06 15:16:43 +0200

    Adding sitemap.xml

---
+++ src/app/sitemap.ts
@@ -0,0 +1,32 @@
+import type { MetadataRoute } from "next";
+
+export const dynamic = "force-static";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+	return [
+		{
+			url: "https://lenguarazfest.com",
+			lastModified: new Date(),
+			changeFrequency: "weekly",
+			priority: 1
+		},
+		{
+			url: "https://lenguarazfest.com/el-festival",
+			lastModified: new Date(),
+			changeFrequency: "weekly",
+			priority: 0.8
+		},
+		{
+			url: "https://lenguarazfest.com/los-libros",
+			lastModified: new Date(),
+			changeFrequency: "weekly",
+			priority: 0.8
+		},
+		{
+			url: "https://lenguarazfest.com/el-programa",
+			lastModified: new Date(),
+			changeFrequency: "weekly",
+			priority: 0.5
+		}
+	];
+}
